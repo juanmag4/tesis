d2dc8bc570130ba2864c65d4fb21a8c504da201d
Martin Navarro <mnavarromugas@hotmail.com>
2018-05-30 20:21:08 -0300

    Fix last crappy merge

--- client/main.js
+++ client/main.js
@@ -1,6 +1,8 @@
+import React from 'react';
 import ReactDOM from 'react-dom';
 import { Meteor } from 'meteor/meteor';
 import Routes from '../imports/routes/routes';
 
 Meteor.startup(() => {
   ReactDOM.render(<Routes />, document.getElementById('app'));
+});
